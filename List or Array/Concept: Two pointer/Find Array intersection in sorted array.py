65e3c1de131d2d6962cf771b9113bbd1f338d46d
Kunal <kunal10713@gmail.com>
2024-10-05 19:26:30 +0530

    Two Pointer

--- List or Array/Concept: Two pointer/Find Array intersection in sorted array.py
+++ List or Array/Concept: Two pointer/Find Array intersection in sorted array.py
@@ -1,43 +1,39 @@
-import sys
-
 def find_array_intersection_sorted(list1, list2, size1, size2):
     ptr1 = 0
     ptr2 = 0
 
     ans = []
     while(ptr1 < size1 and ptr2 < size2):
-        if(list1[ptr1] == list2[ptr2]):
+        # If both elements are equal, add to answer and move both pointers
+        if list1[ptr1] == list2[ptr2]:
             ans.append(list1[ptr1])
-            list2[ptr2] = sys.maxint
             ptr1 += 1
             ptr2 += 1
-            
-        if(list1[ptr1] < list2[ptr2]):
+
+        # Move pointer of the smaller element to try and find a match
+        elif list1[ptr1] < list2[ptr2]:
             ptr1 += 1
-            continue
-        
-        if(list1[ptr1] > list2[ptr2]):
+        else:  # list1[ptr1] > list2[ptr2]
             ptr2 += 1
-            continue
-    
-    print(ans)
-    
+
+    print("Intersection of the two arrays:", ans)
+
 
 # Get the size of the array
 size1 = int(input("Enter size of array 1: "))
 list1 = []  # Initialize an empty list
 list2 = []  # Initialize an empty list
 
-print("Enter the elements of the list 1, Make sure input is sorted:")
+print("Enter the elements of list 1 (Make sure the input is sorted):")
 
 # Take input for each element of array 1
 for x in range(size1):
     element = int(input(f"Element {x + 1}: "))
     list1.append(element)  # Append each element to the list
 
-size2 = int(input("Enter size of array 2: Make sure input is sorted: "))
+size2 = int(input("Enter size of array 2 (Make sure the input is sorted): "))
 
-print("Enter the elements of the list 2:")
+print("Enter the elements of list 2:")
 
 # Take input for each element of array 2
 for x in range(size2):
